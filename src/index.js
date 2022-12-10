3eb152ca1bd7b92b29175e2e799ac49f619c5001
ndaboom <ndabosam084@gmail.com>
2022-12-10 18:46:02 +0100

    Quote & Home pages added

--- src/index.js
+++ src/index.js
@@ -1,27 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/about',
-    element: '<h2> Hi Sam</h2>',
-  },
-]);
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>,
 );
 
